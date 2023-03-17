e533e31b0bda5c69ae2c2f4643109d3e02749c82
subiksha <subikshauchil@gmail.com>
2023-03-17 16:42:07 +0530

    ssss

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -9,44 +9,52 @@ import Loader from "../layout/Loader/Loader";
 import { useAlert } from "react-alert";
 
 const Home = () => {
-    const alert = useAlert();
-    const dispatch = useDispatch();
-    const {loading,error,profiles,profilesCount} = useSelector((state)=>state.profiles)
+	const alert = useAlert();
+	const dispatch = useDispatch();
+	const { loading, error, profiles, profilesCount } = useSelector(
+		(state) => state.profiles
+	);
 
+	useEffect(() => {
+		if (error) {
+			alert.error(error);
+			dispatch(clearErrors());
+		}
+		dispatch(getProfile());
+	}, [dispatch, error, alert]);
 
-    useEffect(() => {
-        if (error) {
-            alert.error(error);
-            dispatch(clearErrors());
-        }
-        dispatch(getProfile());
-    }, [dispatch, error,alert]);
+	return (
+		<Fragment>
+			{loading ? (
+				<Loader />
+			) : (
+				<Fragment>
+					<MetaData title="Sakhiii!!" />
+					<div className="banner">
+						<h1>Welcome to Sakhi - an Online Job Portal Website </h1>
+						<p>
+							{" "}
+							Find the right candidate for all your daily household
+							requirements!!
+						</p>
 
-
-
-    return (
-        <Fragment>
-            {loading?(<Loader />):(
-            <Fragment>
-            <MetaData title="Sakhiii!!"/>
-        <div className="banner">
-                <h1>Welcome to Sakhi - an Online Job Portal Website </h1>
-                <p> Find the right candidate for all your daily household requirements!!</p>
-                
-                <a href="#container">
-                    <button> Scroll
-                        <CgMouse/>
-                    </button>
-                </a>
-            </div>
-            <h2 className="homeHeading">Featured Profiles</h2>
-            <div className="container" id="container">
-                {profiles && profiles.map((profiles)=> <Profile profiles= {profiles}/> )}
-             </div>
-            </Fragment>
-            )}
-        </Fragment>
-    );
+						<a href="#container">
+							<button>
+								{" "}
+								Scroll
+								<CgMouse />
+							</button>
+						</a>
+					</div>
+					<h2 className="homeHeading">Featured Profiles</h2>
+					<div className="container" id="container">
+						{profiles &&
+							profiles.map((profiles) => <Profile profiles={profiles} />)}
+					</div>
+				</Fragment>
+			)}
+		</Fragment>
+	);
 };
 
 export default Home;
