840f0cfa046ab8006594cb87018c68c8cf4c307f
subiksha <subikshauchil@gmail.com>
2023-04-12 17:02:19 +0530

    loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,6 @@
 import "./App.css";
 import Header from "./component/layout/Header/Header.js";
-import {
-	BrowserRouter as Router,
-	Route,
-	Routes,
-	Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WebFont from "webfontloader";
 import React from "react";
 import Footer from "./component/layout/Footer/Footer.js";
